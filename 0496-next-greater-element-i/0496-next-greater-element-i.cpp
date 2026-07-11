722dfac26b95ef57f2cae63fcb64a07066f025ea
Ishaan Sahi <ishaansahi123@gmail.com>
2026-07-11 15:01:29 +0530

    Time: 3 ms (40.99%), Space: 13 MB (15.63%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,27 +1,59 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         int num,len1,len2;
+//         len1=nums1.size();
+//         len2=nums2.size();
+//         vector<int>result;
+//         int idx=len2;
+//         for(int i=0; i<len1; i++){
+//             num = nums1[i];
+//             idx=len2;
+//             for(int j=0;j<len2;j++){
+//                 if(nums2[j]==num){
+//                     idx=j;
+//                 }
+//                 if(nums2[j]<=num && j==len2-1){
+//                     result.push_back(-1);
+//                 }
+//                 if(nums2[j]>num && j>idx){
+//                     result.push_back(nums2[j]);
+//                     break;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int num,len1,len2;
-        len1=nums1.size();
-        len2=nums2.size();
-        vector<int>result;
-        int idx=len2;
-        for(int i=0; i<len1; i++){
-            num = nums1[i];
-            idx=len2;
-            for(int j=0;j<len2;j++){
-                if(nums2[j]==num){
-                    idx=j;
-                }
-                if(nums2[j]<=num && j==len2-1){
-                    result.push_back(-1);
-                }
-                if(nums2[j]>num && j>idx){
-                    result.push_back(nums2[j]);
-                    break;
-                }
+        stack<int> st;
+        unordered_map<int, int> mp;
+
+        // Find next greater element for every element in nums2
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
             }
+
+            if (st.empty())
+                mp[nums2[i]] = -1;
+            else
+                mp[nums2[i]] = st.top();
+
+            st.push(nums2[i]);
         }
-        return result;
+
+        // Build answer for nums1
+        vector<int> ans;
+
+        for (int x : nums1) {
+            ans.push_back(mp[x]);
+        }
+
+        return ans;
     }
 };
